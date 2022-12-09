a7dd3969717593e734302556531bd9f69a70d809
uthcst <115212667+uthcst@users.noreply.github.com>
2022-12-09 22:19:57 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 const express = require('express');
-const port = process.env.port || 4000;
+const bodyParser = require("body-parser");
+const { check, validationResult } = require('express-validator');
+
 const app = express();
-
-app.set('view engine', 'pug');
-app.set('views', __dirname + '/views');
-
-app.use('/css', express.static('css'));
-app.get('/', (request, response) => {
-    response.render('index', {
-      subject: 'Pug template engine',
-      name: 'Pug Example',
-      link: 'http://cs.uth.gr'
-    });
-  });
-app.get("")
-app.listen(port, function () {
-    console.log('Server is running at ', port);
+const port = process.env.port || 4000;
+app.use('/', express.static('www'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.post('/form', [
+  check('name').isLength({ min: 3 }).trim().escape(),
+  check('email').isEmail().normalizeEmail(),
+  check('age').isNumeric().trim().escape()
+], (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() })
+  }
+  const name = req.body.name;
+  const email = req.body.email;
+  const age = req.body.age;
+  res.send(`${name} ${email} ${age} submitted successfully!`)
+});
+const server = app.listen(port, function () {
+  console.log('Server is running at ', port);
 });
